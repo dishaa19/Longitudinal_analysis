f95f936755a2e0158ad05083da3278fc88ac3612
Disha_Purohit <46378274+dishaa19@users.noreply.github.com>
2020-06-04 14:40:37 +0200

    Update and rename XGboost (leave one out).py to XGboost (leave one out cross validation).py

--- XGboost (leave one out cross validation).py
+++ XGboost (leave one out cross validation).py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[17]:
-
-
 import pandas as pd
 import numpy as np
 from xgboost.sklearn import XGBRegressor
@@ -19,8 +14,6 @@ from scipy.stats import spearmanr
 import math
 
 
-# In[18]:
-
 
 df_1 = pd.read_csv('ip_5.csv',header=0)
 y = pd.read_csv('weeks.csv',header = 0)
@@ -28,26 +21,14 @@ df = df_1.transpose()
 df = df[1:]
 X = df
 
-
-# In[19]:
-
-
 median_pred = pd.DataFrame({"patient_id": X.index})
 y = pd.concat([median_pred, y], axis=1)
 y['patient_id'] = y['patient_id'].astype(str)
 Y = y.set_index('patient_id')
 Y = Y["weeks"]
-Y
-
-
-# In[20]:
-
 
 X["p"] = [x.split("_")[0] for x in X.index]
 y["p"] = [x.split("_")[0] for x in Y.index]
-
-
-# In[21]:
 
 
 #reg:linear
@@ -56,10 +37,6 @@ prediction = []
 i = 0
 scaler = MinMaxScaler(feature_range=(0, 1))
 sc = StandardScaler()
-
-
-# In[22]:
-
 
 for patient in X["p"].unique():
     X_val = X.loc[X.p==patient]
@@ -71,10 +48,7 @@ for patient in X["p"].unique():
     pred_old = pd.DataFrame({"patient_id": X_train.index})
     X_train = sc.fit_transform(X_train)
     X_val = sc.fit_transform(X_val)
-    #data_dmatrix = xgb.DMatrix(data=X,label=y)
-    #X_train, y_train = make_regression(n_samples=len(X_train), n_features=len(X.columns)-1)
-    #X_val, y_val = make_regression(n_samples=len(X_val), n_features=len(X.columns)-1)
-        
+    
     xgb1 = XGBRegressor()
     grid = GridSearchCV(xgb1,
                         param_grid,
@@ -91,10 +65,6 @@ for patient in X["p"].unique():
     i += 1
     
 
-
-# In[23]:
-
-
 print("Length of prediction:",len(prediction))
 print("Length of y_val:",len(y_val))
 print("Prediction:",prediction)
@@ -102,10 +72,6 @@ print("Prediction:",prediction)
 y_true = y.sort_values("patient_id")
 y_true = np.array(y_true.weeks)
 y_val  = y_true
-
-
-# In[24]:
-
 
 mse = mean_squared_error(y_val,prediction)
 print('Mean squared error:', mse)
@@ -120,9 +86,4 @@ print( "%.16f" % float(p))
 log = -(math.log10(p))
 print("-Log(p_val):",log)
 
-
-# In[ ]:
-
-
-[9.98,0.13,0.089,10.19,0.90,27.17,27.79]
 
